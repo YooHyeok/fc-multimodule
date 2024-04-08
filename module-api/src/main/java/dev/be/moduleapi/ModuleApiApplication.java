b5dcc057f932cf748ec13128adfdc4462c4e528d
webdevyoo <yjk7454@naver.com>
2024-04-08 20:11:17 +0900

    @SpringBootApplication(scanBasePackages) 방식의 서로 다른 모듈간 컴포넌트 스캔 적용

---
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -0,0 +1,17 @@
+package dev.be.moduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(
+        /* 모든 모듈을 다 스캔하는 것은 시간도 오래걸리고 굉장히 비효율적이기 때문에, 빈으로 등록해야 되는 필요한 것들만 명시한다. */
+        scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}
+)
+public class ModuleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApplication.class, args);
+    }
+
+}
