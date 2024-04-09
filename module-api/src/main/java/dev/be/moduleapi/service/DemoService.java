4e53fb8a3dc7bde7953e58c57334e085d9136fe0
YooHyeok <yjk7454@naver.com>
2024-04-09 12:23:50 +0900

    DB연동 - Service save, find 기능에 실제 JPA 조작 기능 구현

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,10 +1,13 @@
 package dev.be.moduleapi.service;
 
 import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repository.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +15,20 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
     private final CommonDemoService demoService;
+    private final MemberRepository memberRepository;
+
     public String save() {
-        System.out.println(CodeEnum.SUCCESS.getCode());
-        System.out.println(demoService.commenService());
+//        System.out.println(CodeEnum.SUCCESS.getCode());
+//        System.out.println(demoService.commenService());
+        memberRepository.save(Member.builder()
+                .name(Thread.currentThread().getName())
+                .build());
         return "save";
     }
 
     public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println("DB size = " + size);
         return "find";
     }
 
