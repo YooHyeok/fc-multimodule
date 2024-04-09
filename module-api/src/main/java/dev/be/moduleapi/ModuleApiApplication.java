52f4a5199a860a65508488ca2a50cb09e3f8aa87
YooHyeok <yjk7454@naver.com>
2024-04-09 12:27:46 +0900

    DB연동 - 서로 다른 모듈간 Repository, Entity에 대한 도메인 패키지 공유 설정
    Spring을 기동하는 Application 클레스의 클레스레벨에 어노테이션을 통해 설정한다.
    
    @EnableJpaRepositories로 아래 오류 해결
    Parameter 1 of constructor in dev.be.moduleapi.service.DemoService required a bean of type 'dev.be.modulecommon.repository.MemberRepository' that could not be found.
    @EntityScan으로 아래오류 해결
    Not a managed type: class dev.be.modulecommon.domain.Member

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -2,12 +2,16 @@ package dev.be.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(
         /* 모든 모듈을 다 스캔하는 것은 시간도 오래걸리고 굉장히 비효율적이기 때문에, 빈으로 등록해야 되는 필요한 것들만 명시한다. */
         scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}
 )
+@EntityScan("dev.be.modulecommon.domain")
+@EnableJpaRepositories(basePackages = "dev.be.modulecommon.repository")
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
